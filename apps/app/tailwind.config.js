1c52a12d10961e304cada60b5044aa2a44d1455a
Shikakka <y.tintel@gmail.com>
2021-07-21 11:58:32 +0200

    cleanup tailwind app config

--- apps/app/tailwind.config.js
+++ apps/app/tailwind.config.js
@@ -2,17 +2,9 @@ const { createGlobPatternsForDependencies } = require('@nrwl/react/tailwind');
 const { join } = require('path');
 
 module.exports = {
-  presets: [require('../../tailwind-workspace-preset.js')],
-  purge: [
-    join(__dirname, '**/*.{js,ts,jsx,tsx}'),
-    ...createGlobPatternsForDependencies(__dirname),
-  ],
-  darkMode: 'media', // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+    presets: [require('../../tailwind-workspace-preset.js')],
+    purge: [
+        join(__dirname, '**/*.{js,ts,jsx,tsx}'),
+        ...createGlobPatternsForDependencies(__dirname),
+    ]
 };
--- tailwind-workspace-preset.js
+++ tailwind-workspace-preset.js
@@ -1,10 +1,12 @@
 module.exports = {
     mode: 'jit',
+    darkMode: 'media', // or 'media' or 'class'
     theme: {
         extend: {},
     },
     variants: {
         extend: {},
     },
+    plugins: [],
     plugins: [require('@tailwindcss/typography')],
 };
